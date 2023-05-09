3312614fe6a32fb0834fccd19e06cf70e4aa4820
Aman07a <aman16.aa17@gmail.com>
2023-05-09 14:19:24 +0200

    Lifecycle Functions Practical Example
    Binding Dimensions

--- src/lib/TodoList.svelte
+++ src/lib/TodoList.svelte
@@ -27,11 +27,18 @@
     }
   });
   afterUpdate(() => {
-    console.log(listDiv.offsetHeight);
+    if (autoscroll) listDiv.scrollTo(0, listDivScrollHeight);
+    autoscroll = false;
   });
 
   export let todos = [];
-  export const readonly = "read only";
+  let prevTodos = todos;
+
+  $: {
+    autoscroll = todos.length > prevTodos.length;
+    prevTodos = todos;
+  }
+
   export function clearInput() {
     inputText = "";
   }
@@ -39,7 +46,7 @@
     input.focus();
   }
   let inputText = "";
-  let input, listDiv;
+  let input, listDiv, autoscroll, listDivScrollHeight;
 
   const dispatch = createEventDispatcher();
 
@@ -72,27 +79,36 @@
 
 <div class="todo-list-wrapper">
   <div class="todo-list" bind:this={listDiv}>
-    <ul>
-      {#each todos as { id, title, completed } (id)}
-        <li>
-          <label>
-            <input
-              on:input={(event) => {
-                event.currentTarget.checked = completed;
-                handleToggleTodo(id, !completed);
-              }}
-              type="checkbox"
-              checked={completed}
-            />
-            {title}
-          </label>
-          <button on:click={() => handleRemoveTodo(id)}>Remove</button>
-        </li>
-      {/each}
-    </ul>
+    <div bind:offsetHeight={listDivScrollHeight}>
+      <ul>
+        {#each todos as { id, title, completed } (id)}
+          <li>
+            <label>
+              <input
+                on:input={(event) => {
+                  event.currentTarget.checked = completed;
+                  handleToggleTodo(id, !completed);
+                }}
+                type="checkbox"
+                checked={completed}
+              />
+              {title}
+            </label>
+            <button on:click={() => handleRemoveTodo(id)}>Remove</button>
+          </li>
+        {/each}
+      </ul>
+    </div>
   </div>
   <form class="add-todo-form" on:submit|preventDefault={handleAddTodo}>
     <input bind:this={input} bind:value={inputText} />
     <Button type="submit" disabled={!inputText}>Add</Button>
   </form>
 </div>
+
+<style>
+  .todo-list {
+    max-height: 150px;
+    overflow: auto;
+  }
+</style>
