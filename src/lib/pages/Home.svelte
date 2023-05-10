f2eae6ce1c867f13c3915c458c9539116fa4b0a5
Aman07a <aman16.aa17@gmail.com>
2023-05-10 18:15:05 +0200

    Auto-subscriptions & Store Binding

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,15 +1,7 @@
 <script>
-  import { onDestroy } from 'svelte';
-  import settings from '../stores/setttings';
-  let colorScheme;
-  const unsubscribe = settings.subscribe((settings) => {
-    console.log('home subscribe');
-    colorScheme = settings.colorScheme;
-  });
-
-  onDestroy(unsubscribe);
+  import settings from '../stores/settings';
 </script>
 
 <h2>Home</h2>
 
-{colorScheme}
+{$settings.colorScheme}
