8fc75ab4c43b61caf921d0aceabe9768f7a05380
Aman07a <aman16.aa17@gmail.com>
2023-05-12 11:41:50 +0200

    Handling Updating Konva.js Components

--- src/lib/konva/Layer.svelte
+++ src/lib/konva/Layer.svelte
@@ -1,11 +1,19 @@
 <script>
   import Konva from 'konva';
-  import { getContext, setContext } from 'svelte';
+  import { getContext, onDestroy, setContext } from 'svelte';
   import { stageKey, layerKey } from './context-keys';
+
+  // export let draggable = undefined;
 
   const { getStage } = getContext(stageKey);
   const stage = getStage();
-  const layer = new Konva.Layer();
+  const layer = new Konva.Layer($$props);
+
+  $: layer.setAttrs($$props);
+
+  onDestroy(() => {
+    layer.destroy();
+  });
 
   setContext(layerKey, layer);
 
