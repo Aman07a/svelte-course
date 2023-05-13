cfb755dd81dcd9df4106c1fcbe2b3b2a64d627c5
Aman07a <aman16.aa17@gmail.com>
2023-05-13 17:56:59 +0200

    Changing Layouts Conditionally

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+{#if $page.url.pathname.startsWith('/setting')}
+	<slot />
+{:else}
+	<div style:background="#f03" style:padding="10px">
+		<h3>Root Layout</h3>
+		<nav>
+			<a href="/">Home</a>
+			<a href="/about">About</a>
+			<a href="/settings">Settings</a>
+		</nav>
+		<slot />
+
+		<footer style:margin-top="60px">All rights reserved.</footer>
+	</div>
+{/if}
