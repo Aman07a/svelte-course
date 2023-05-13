70983e4558039042211bfbd9bf2de6182135857f
Aman07a <aman16.aa17@gmail.com>
2023-05-13 15:59:21 +0200

    Navigation Functions: goto(), beforeNavigate() & afterNavigate()

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,15 @@
 <script>
+	import { goto } from '$app/navigation';
+
 	let name = 'Aman';
 </script>
 
 <h1>About {name}</h1>
+
+<button
+	on:click={() => {
+		goto('/');
+	}}>Go to Home</button
+>
 
 <style></style>
