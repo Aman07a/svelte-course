925ce653e9ad78a91ddf7702af7ddda0c5925076
Aman07a <aman16.aa17@gmail.com>
2023-05-14 10:44:01 +0200

    Type-safety with Generated Types

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,7 +4,9 @@ declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
-		// interface PageData {}
+		// interface PageData {
+		// 	products?: { id: number }[];
+		// }
 		// interface Platform {}
 	}
 }
